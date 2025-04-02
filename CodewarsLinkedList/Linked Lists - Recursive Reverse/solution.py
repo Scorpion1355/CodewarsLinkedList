dbe619755d58ddf06720b4af04ef508e9e74cb38
Yulian Zaiats <yulianzaiats@gmail.com>
2025-04-02 20:19:20 +0300

    fixed some issues

--- CodewarsLinkedList/Linked Lists - Recursive Reverse/solution.py
+++ CodewarsLinkedList/Linked Lists - Recursive Reverse/solution.py
@@ -2,10 +2,10 @@ def reverse(head):
     # your code goes here.
     if head is None or head.next is None:
         return head
-    
+
     linked_list = reverse(head.next)
-    
+
     head.next.next = head
     head.next = None
-    
+
     return linked_list
